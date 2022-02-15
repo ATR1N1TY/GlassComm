b3220fbab5f076cefc79a95e6ff462a56e42a54d
unknown <saba4inasaridze@gmail.com>
2022-02-16 03:05:54 +0400

    this is last commit for now, changed bg gradient to index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import styles from "../styles/Home.module.css";
 import Navbar from "../components/navbar";
+import ProductList from "../components/productList";
 
 const Home = () => {
   return (
-    <div className="app bg-gradient-to-tr from-purple-500 to-pink-500 h-screen">
+    <div className="app bg-gradient-to-tr from-red-300 to-red-700 h-screen">
       <Navbar />
+      <ProductList />
     </div>
   );
 };
