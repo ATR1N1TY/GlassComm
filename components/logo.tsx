33dca571b6a9559c62389c9e3c00c4a8c118301e
unknown <saba4inasaridze@gmail.com>
2022-02-16 02:51:41 +0400

    made seperate logo component for many usecases

---
+++ components/logo.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="name text-3xl  ">
+      <div className="logo w-full h-2 flex ">
+        <div className="orange w-1/2 h-full bg-amber-900 "></div>
+        <div className="blue w-1/2 h-full bg-yellow-200 "></div>
+      </div>
+      GlassComm
+    </div>
+  );
+};
+
+export default Logo;
