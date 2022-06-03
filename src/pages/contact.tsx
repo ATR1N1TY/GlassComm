27d8fec528bbb8fec2e79ecdf6d9a597f3f9f29b
unknown <saba4inasaridze@gmail.com>
2022-06-03 16:54:57 +0400

    added contact information

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,7 +1,37 @@
 import React from "react";
+import Navbar from "../components/navbar/navbar";
+import { AiFillYoutube, AiFillLinkedin } from "react-icons/ai";
+import { BsFacebook } from "react-icons/bs";
+import logo from "../../public/LOGO.jpg";
+import Image from "next/image";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="bg-gradient-to-tr h-screen from-red-300 to-red-600 dark:from-zinc-900 dark:to-slate-900">
+      <Navbar />
+      <div className="wrap flex items-center justify-center flex-col h-5/6 ">
+        <p className=" text-5xl mb-16 dark:text-indigo-50">FIND ME ON</p>
+        <div className="socialPlatforms flex text-black dark:text-indigo-50">
+          <BsFacebook className=" w-64 h-64 hover:text-blue-500" />
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.youtube.com/indiespace"
+          >
+            <AiFillYoutube className=" w-64 h-64 hover:text-red-600" />
+          </a>
+          <Image
+            className="a3Logo rounded-full hover:animate-spin"
+            width={256}
+            height={256}
+            src={logo}
+            alt="atrinity logo"
+          />
+          <AiFillLinkedin className=" w-64 h-64 hover:text-blue-800" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
