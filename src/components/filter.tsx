c9ecc0968694743f07d430d9bda7096125c86c1a
unknown <saba4inasaridze@gmail.com>
2022-06-03 16:48:15 +0400

    added responsiveness

--- src/components/filter.tsx
+++ src/components/filter.tsx
@@ -16,18 +16,18 @@ const Filter = () => {
     },
     filterDispatch,
   } = useContext(globalContext);
-  // console.log(include_out_of_stock, include_fast_delivery_only);
-
-  // console.log(by_ratings);
 
   return (
-    <div className="glass m-4 mt-8 p-4 rounded-full text-pink-50 text-base inline-flex items-center">
-      {/* search */}
-      <form action="filter flex">
+    <div className="glass bgGlass xl:static my-8 p-4 xl:m-4 xl:mt-8 xl:p-4 rounded-2xl xl:rounded-full  text-pink-50 text-base xl:inline-flex xl:justify-center xl:items-center 2xl:items-center  xl:w-auto">
+      <form
+        action="filter"
+        className="flex xl:w-auto flex-col xl:flex-row xl:h-10 xl:items-center justify-evenly "
+      >
+        {/* searchbar */}
         <input
           name="search"
           type="text"
-          className="search glass rounded-full py-2 pl-4 pr-16 placeholder:text-pink-100"
+          className="search glass rounded-full px-4 py-2 xl:pl-4 2xl:py-2 2xl:pl-4 xl:pr-16 placeholder:text-pink-100 dark:placeholder:text-gray-800"
           placeholder="Search food..."
           onChange={(e) =>
             filterDispatch({
@@ -36,43 +36,46 @@ const Filter = () => {
             })
           }
         />
-        {/* <label htmlFor="search" className="ml-4">
-          <HiOutlineSearchCircle
-            className="inline h-full text-pink-200 hover:cursor-pointer "
-            size={35}
+
+        {/* price selectors */}
+        <div className="priceFilter my-4 xl:my-0">
+          <label
+            htmlFor="price"
+            className="xl:ml-4 font-bold justify-evenly xl:hidden 2xl:inline"
+          >
+            Price:
+          </label>
+          <input
+            name="price"
+            type="text"
+            className="glass rounded-full xl:w-24 xl:mx-4 p-2 placeholder:text-pink-100  text-center m-4 xl:m-0"
+            placeholder="MIN"
+            onChange={(e) =>
+              filterDispatch({
+                type: FILTER_ACTIONS.REMOVE_PRICES_BELOW,
+                payload: e.target.value,
+              })
+            }
           />
-        </label> */}
+          <input
+            name="price"
+            type="text"
+            className="glass rounded-full  xl:w-24 mt-2 p-2  placeholder:text-pink-100 text-center m-4 xl:m-0"
+            placeholder="MAX"
+            onChange={(e) =>
+              filterDispatch({
+                type: FILTER_ACTIONS.REMOVE_PRICES_UPPER,
+                payload: e.target.value,
+              })
+            }
+          />
+        </div>
 
-        <label htmlFor="price" className="ml-4 font-bold">
-          Price:
-        </label>
-        <input
-          name="price"
-          type="text"
-          className="glass rounded-full mx-4 p-2  placeholder:text-pink-100 w-24 text-center"
-          placeholder="MIN"
-          onChange={(e) =>
-            filterDispatch({
-              type: FILTER_ACTIONS.REMOVE_PRICES_BELOW,
-              payload: e.target.value,
-            })
-          }
-        />
-        <input
-          name="price"
-          type="text"
-          className="glass rounded-full p-2 w-24 placeholder:text-pink-100 text-center "
-          placeholder="MAX"
-          onChange={(e) =>
-            filterDispatch({
-              type: FILTER_ACTIONS.REMOVE_PRICES_UPPER,
-              payload: e.target.value,
-            })
-          }
-        />
-
-        <div className="ml-4 inline font-bold mr-4">Order:</div>
-        <label htmlFor="desc">
+        {/* price order: ascending or descending */}
+        <div className="xl:ml-4 inline font-bold xl:m-4 xl:my-4 xl:inline xl:hidden 2xl:inline ">
+          Order:
+        </div>
+        <label htmlFor="desc" className="xl:ml-2">
           Ascending
           <input
             type="radio"
@@ -101,13 +104,14 @@ const Filter = () => {
           />
         </label>
 
-        <label htmlFor="byStock" className=" font-bold ml-2">
+        {/* filter by stock */}
+        <label htmlFor="byStock" className=" font-bold mt-4 xl:ml-2 xl:my-4">
           Include Out of Stock
         </label>
         <input
           type="checkbox"
           name="byStock"
-          className="ml-2"
+          className="xl:ml-2"
           checked={include_out_of_stock}
           onChange={() =>
             filterDispatch({
@@ -116,14 +120,17 @@ const Filter = () => {
             })
           }
         />
-
-        <label htmlFor="byFastShipping" className=" font-bold ml-4">
+        {/* include fast delivery */}
+        <label
+          htmlFor="byFastShipping"
+          className="mt-4 xl:mt-0 font-bold xl:ml-4"
+        >
           Fast Delivery
         </label>
         <input
           type="checkbox"
           name="byFastShipping"
-          className="ml-2"
+          className="xl:ml-2 xl:m-4"
           checked={include_fast_delivery_only}
           onChange={() =>
             filterDispatch({
@@ -133,7 +140,15 @@ const Filter = () => {
           }
         />
 
-        <RatingComponent productRating={by_ratings} dispatch={filterDispatch} />
+        {/* filter by ratings */}
+        <div className="rating mt-4 xl:mt-0 xl:inline">
+          <RatingComponent
+            productRating={by_ratings}
+            dispatch={filterDispatch}
+          />
+        </div>
+
+        {/* clear filter */}
         <div
           className="clear-filter inline hover:cursor-pointer ml-4"
           onClick={() => filterDispatch({ type: FILTER_ACTIONS.CLEAR_FILTER })}
@@ -141,11 +156,6 @@ const Filter = () => {
           <FcClearFilters size={30} className="inline" />
         </div>
       </form>
-
-      {/* idea: როდესაც მომხმარებელი ქვემოთ ჩამოსქროლავს და ფილტერ კომპონენტი აღარ იქნება ეკრანზე,
-      გადმოვიდეს ეს კომპონენტი ეკრანის მარცხნივ და პროდუქტლისტ კომპონენტმა ჩაიჩოჩოს მარჯვნივ, 
-      ეს ყველაფერი ანიმაციით,
-      ასევე ფილტერ კომპონენტმა უნდა გახდეს ვერტიკალური */}
     </div>
   );
 };
