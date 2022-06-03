5162d3735b2082b311aef23ff9270003d9fccbb0
unknown <saba4inasaridze@gmail.com>
2022-06-03 15:54:19 +0400

    added signIn component page

---
+++ src/pages/signIn.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Navbar from "../components/navbar/navbar";
+
+const signIn = () => {
+  return (
+    <div className=" w-screen h-screen bg-gradient-to-tr from-red-300 to-red-600 dark:from-zinc-900 dark:to-slate-900">
+      <Navbar />
+      <main className="flex items-center justify-center mt-16">
+        <div className="wrapper glass p-8 rounded-2xl ">
+          <form action="sign-in flex flex-col ">
+            <div className="nameWrap mt-4">
+              <label className="mr-2" htmlFor="username">
+                Username
+              </label>
+              <input className="glass rounded-md" name="username" type="text" />
+            </div>
+
+            <div className="passWrap mt-4">
+              <label className="mr-2" htmlFor="password">
+                Password
+              </label>
+              <input
+                className="glass rounded-md"
+                name="password"
+                type="password"
+              />
+            </div>
+
+            <p className="mt-8">Forgot your password?</p>
+            <p>Register</p>
+
+            <button className="loginBtn glass mt-8 w-full rounded-md">
+              Log in
+            </button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default signIn;
