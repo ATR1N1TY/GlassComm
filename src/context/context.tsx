2fc380a67737405407c7625d68e774ee3b42dc8f
unknown <saba4inasaridze@gmail.com>
2022-06-03 17:00:49 +0400

    reduced line count from ~150 to ~70 by creating custom hooks

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -1,125 +1,62 @@
-import React, { useEffect, useState } from "react";
-import { createContext, useReducer } from "react";
-import { faker } from "@faker-js/faker";
-import { globalReducer, filterReducer, currencyReducer } from "./reducers";
+import React, { useEffect, useState, createContext, useReducer } from "react";
+import { globalReducer, filterReducer } from "./reducers";
 import { productList } from "./data";
+import { filterInitialState } from "./initialStates";
+import { storeInitialState, Product } from "../types/types";
+import useDarkTheme from "../hooks/useDarkTheme";
+import useExchangeRates from "../hooks/useExchangeRates";
+// import { faker } from "@faker-js/faker";
 
 interface GlobalContext {}
 
 const initialState: GlobalContext = {};
 
-// კონტექსტი
 export const globalContext = createContext<any>(initialState);
 
-// TS ინტერფეისები
-export interface Product {
-  id: string;
-  name: string;
-  price: string;
-  images: string[];
-  inStock: number;
-  fastDelivery: boolean;
-  ratings: number;
-  description: string;
-  quantity: number | null;
-}
-
-export interface storeInitialState {
-  products: Product[];
-  cart: [];
-  subtotal: number;
-}
-
-export interface filterState {
-  search_query: string;
-  min_price: number;
-  max_price: number;
-  by_order: string | null;
-  include_out_of_stock: boolean;
-  include_fast_delivery_only: boolean;
-  by_ratings: null | number;
-}
-
 export const GlobalContextProvider = ({ children }: any) => {
-  //პროდუქტების მასივი
   const [products, setProducts] = useState(productList);
+  const [showMenu, setShowMenu] = useState(false);
+  const [filterOn, setFilterOn] = useState(false);
+  const [currencyData, setCurrencyData, rates] = useExchangeRates();
+  const [darkTheme, setDarkTheme] = useDarkTheme();
 
-  //fetchCurrency ფუნქციიდან წამოსული კურსი
-  const [rate, setRate] = useState(1);
-
-  // ფუნქცია რომელიც იღებს რომელ ვალუტაში გარდაქმნას რომელი ვალუტა
-  const fetchCurrency = (to: string, from: string) => {
-    console.log("from: " + from, "to: " + to);
-
-    fetch(
-      `https://api.apilayer.com/exchangerates_data/convert?to=${to}&from=${from}&amount=1`,
-      {
-        headers: {
-          apikey: "z8buYjkFfCEXpkjz9A3ANbMagFWSxLVo",
-        },
-      }
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        const { result } = data;
-        setRate(result);
-      })
-      .catch((error) => console.log("error", error));
-  };
-
-  // გლობალსთეითის პირველი სახე
   const globalInitialState: storeInitialState = {
     products: products,
     cart: [],
     subtotal: 0,
   };
 
-  //ფილტერსთეითის პირველი სახე
-  const filterInitialState: filterState = {
-    search_query: "",
-    min_price: 1,
-    max_price: 99999999999999,
-    by_order: null, //null | "highToLow" | "lowToHigh"
-    include_out_of_stock: true,
-    include_fast_delivery_only: false,
-    by_ratings: 3,
-  };
-
-  const currencytInitialState = {
-    to: "USD",
-    from: "USD",
-    // rate: 1,
-  };
-
-  // გლობალ სთეითის რედიუსერ ჰუკი
   const [globalState, globalDispatch] = useReducer(
     globalReducer,
     globalInitialState
   );
 
-  //ფილტრესთეითის რედიუსერ ჰუკი
   const [filterState, filterDispatch] = useReducer(
     filterReducer,
     filterInitialState
   );
 
-  const [currencyState, currencyDispatch] = useReducer(
-    currencyReducer,
-    currencytInitialState
-  );
-
-  //ჰუკი რომელიც მაშინ გაეშვება როდესაც შეიცვლება
   useEffect(() => {
-    fetchCurrency(currencyState.to, currencyState.from);
-    console.log(rate);
+    if (!Object.keys(rates).length) return;
 
     setProducts(
       globalState.products.filter(
         (product: Product) =>
-          (product.price = (Number(product.price) * rate).toString())
+          (product.price = Math.round(
+            (Number(product.price) / rates[currencyData.from]) *
+              rates[currencyData.to]
+          ).toString())
       )
     );
-  }, [currencyState.to]);
+
+    globalState.cart.filter(
+      (product: Product) =>
+        (product.price = Math.round(
+          (Number(product.price) / rates[currencyData.from]) *
+            rates[currencyData.to]
+        ).toString())
+    );
+  }, [rates, currencyData]);
 
   return (
     <globalContext.Provider
@@ -128,8 +65,15 @@ export const GlobalContextProvider = ({ children }: any) => {
         globalDispatch,
         filterState,
         filterDispatch,
-        currencyDispatch,
-        rate,
+        currencyData,
+        setCurrencyData,
+        rates,
+        darkTheme,
+        setDarkTheme,
+        filterOn,
+        setFilterOn,
+        showMenu,
+        setShowMenu,
       }}
     >
       {children}
