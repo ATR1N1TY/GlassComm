4ec2831a92c1c3a53da394fc135d048e46f58a46
unknown <saba4inasaridze@gmail.com>
2022-06-16 17:08:53 +0400

    added responsive design to contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -13,24 +13,27 @@ const Contact = () => {
       <HamburgerMenu />
 
       <div className="wrap flex items-center justify-center flex-col h-5/6 ">
-        <p className=" text-5xl mb-16 dark:text-indigo-50">FIND ME ON</p>
-        <div className="socialPlatforms flex text-black dark:text-indigo-50">
-          <BsFacebook className=" w-64 h-64 hover:text-blue-500" />
+        <p className=" text-5xl  lg:mb-16 dark:text-indigo-50">FIND ME ON</p>
+        <div className="socialPlatforms flex flex-col items-center md:flex-row text-black dark:text-indigo-50">
+          <div className="margn m-4 w-32 h-32 lg:w-64 lg:h-64">
+            <Image
+              className="a3Logo rounded-full hover:animate-spin"
+              width={256}
+              height={256}
+              src={logo}
+              alt="atrinity logo"
+            />
+          </div>
+          <BsFacebook className="w-32 h-32 lg:w-64 lg:h-64 hover:text-blue-500" />
           <a
             target="_blank"
             rel="noreferrer"
             href="https://www.youtube.com/indiespace"
+            className="block"
           >
-            <AiFillYoutube className=" w-64 h-64 hover:text-red-600" />
+            <AiFillYoutube className="w-32 h-32 lg:w-64 lg:h-64 hover:text-red-600 " />
           </a>
-          <Image
-            className="a3Logo rounded-full hover:animate-spin"
-            width={256}
-            height={256}
-            src={logo}
-            alt="atrinity logo"
-          />
-          <AiFillLinkedin className=" w-64 h-64 hover:text-blue-800" />
+          <AiFillLinkedin className="w-32 h-32 lg:w-64 lg:h-64 hover:text-blue-800 block" />
         </div>
       </div>
     </div>
